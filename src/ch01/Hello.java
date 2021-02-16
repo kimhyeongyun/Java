92d7548674289a84f5e335d1cd516e95bfea5d0b
kimhyeongyun <ske011217@gmail.com>
2021-02-16 15:21:50 +0900

    자바5678 삭제

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -7,18 +7,7 @@ public class Hello {
 		System.out.println("Hello Java!2");
 		System.out.println("Hello Java!3");
 		System.out.println("Hello Java!4");
-		System.out.println("Hello Java!5");
-		System.out.println("Hello Java!6");
-		System.out.println("Hello Java!7");
-		System.out.println("Hello Java!9");
-		System.out.println("Hello Java!8");
-		System.out.println("Hello Java!8");
-		System.out.println("Hello Java!8");
-<<<<<<< HEAD
-		System.out.println("Hello Java!11");
-=======
-		System.out.println("Hello Java!10");
->>>>>>> branch 'master' of https://github.com/kimhyeongyun/Java
+		
 	}
 
 }
