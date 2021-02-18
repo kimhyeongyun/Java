b7e2061025ed495069f8261b6bac351b6aaa9298
kimhyeongyun <ske011217@gmail.com>
2021-02-18 16:21:12 +0900

    ad

--- src/ch05homework/TeamProject01.java
+++ src/ch05homework/TeamProject01.java
@@ -25,13 +25,20 @@ public class TeamProject01 {
 			int selectNo = Integer.parseInt(scanner.nextLine());
 
 			if(selectNo == 1) { // 1을 읽었을 경우		
-				System.out.println("---------------------------------------------------------------------------------");
-				System.out.println("번호         제목                      내용                        글쓴이        조회수");
-				System.out.println("---------------------------------------------------------------------------------");
-				System.out.print(sentenceNum + "         " + title + "                      " + content + "                        " + writer + "        " + readNum );
-				sentenceNum = Integer.parseInt(scanner.nextLine());
-				scores = new int[sentenceNum];
 				
+					System.out.println("---------------------------------------------------------------------------------");
+					System.out.println("번호         제목                      내용                        글쓴이        조회수");
+					System.out.println("---------------------------------------------------------------------------------");
+				for(int i=0; i < boardArray.length; i++) {
+					
+					System.out.println(sentenceNum + "         " + title + "                      " + content + "                        " + writer + "        " + readNum );
+				}
+				
+				System.out.print("메뉴선택> ");
+				sentenceNum = Integer.parseInt(scanner.nextLine());
+
+			
+			
 			} else if (selectNo == 2) { // 2. 생성(Create)
 				for(int i=0; i < boardArray[0].length-2; i++) {
 					System.out.print("제목: ");
@@ -42,27 +49,27 @@ public class TeamProject01 {
 					writer = scanner.nextLine();
 					
 					
-					scores[i] = Integer.parseInt(scanner.nextLine());
+					//scores[i] = Integer.parseInt(scanner.nextLine());
 				}
 				//Integer.scanner.nextInt();
 			} else if (selectNo == 3) { // 3. 읽기(Read)
 				for(int i=0; i < boardArray.length; i++) {
-					System.out.println("scores[" + i + "]> " + boardArray[i]);	
+					System.out.println("scores[" + i + "]> " + boardArray[i]);	}
 				//Integer.scanner.nextInt();
 			} else if (selectNo == 4) { // 4. 수정(Update)
 				int max = 0;
 				int sum = 0;
 				double avg = 0;
 				for(int i=0; i < boardArray.length; i++) {
-					sum += scores[i];
-					if(max < scores[i]) {
-						max = scores[i];
-					}
+					//sum += scores[i];
+					//if(max < scores[i]) {
+						//max = scores[i];
+					//}
 				}
-				avg = (double) sum / boardArray.length;
-				System.out.println("최고 점수:" + max);
+				//avg = (double) sum / boardArray.length;
+				//System.out.println("최고 점수:" + max);
 				
-				System.out.println("평균 점수:" + avg);
+				//System.out.println("평균 점수:" + avg);
 				//Integer.scanner.nextInt();
 				
 			} else if (selectNo == 5) { // 5. 삭제(Delete)
