0c7a61a093ece1e6e20831c7f36c6131b851c910
kimhyeongyun <ske011217@gmail.com>
2021-02-25 11:11:15 +0900

    2/25 11:11

---
+++ src/ch11/exam01/Example.java
@@ -0,0 +1,19 @@
+package ch11.exam01;
+
+public class Example {
+
+	
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		Member m1 = new Member("winter", "´Ê°Ü¿ï");
+		Member m2 = new Member("winter", "´Ê°Ü¿ï");
+		
+		System.out.println(m1 == m2);
+		System.out.println(m1.equals(m2));
+		
+	}
+		
+		
+
+}
---
+++ src/ch11/exam01/Member.java
@@ -0,0 +1,28 @@
+package ch11.exam01;
+
+public class Member {
+	private String id;
+	private String name;
+	public Member(String id, String name) {
+		// super(); 생략가능
+		this.id = id;
+		this.name = name;
+	}
+	
+	// equals() 재정의
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member ) { // 멤버의 클래스로 만들어졌으면
+			Member m = (Member) obj;
+			if(id.equals(m.id) && name.equals(m.name)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+		
+	}
+	
+}
